fbbe94e2ce936e9f646c267c9c7eea4b84a52218
kadamyash <yash11398@gmail.com>
2022-04-26 00:09:25 -0400

    basic images and singles listed

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const hero_image = document.querySelector("#hero-image");
 
 hero_image.addEventListener("mousemove", (e) => {
-    hero_image.style.backgroundPositionX = (-e.offsetX*0.1) + "px";
+    hero_image.style.backgroundPositionX = (-e.offsetX*0.05) + "px";
 });
